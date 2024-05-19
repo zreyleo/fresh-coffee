b5fec67226c6e10b507c2e31ac27bd6165cc95b2
Regynald Zambrano <regynald.zambrano@priverion.com>
2024-05-19 13:43:39 -0500

    register page

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -2,9 +2,14 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
-        AuthLayout
-        <Outlet />
-    </div>
+    <main
+        className='max-w-4xl m-auto mt-10 md:mt-28 flex flex-col'
+    >
+        <img src="../img/logo.svg" alt="logo" className='max-w-xs' />
+
+        <div>
+            <Outlet />
+        </div>
+    </main>
   )
 }
